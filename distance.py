8da76cdbd8e80cec421ae2cebb92e242f6cde2d9
iswyg <mr_nikit0s@live.ru>
2019-12-19 15:31:37 +0300

    add comment

--- distance.py
+++ distance.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from datetime import datetime
 import time
  
 GPIO.setmode(GPIO.BCM)
  
-#Set trigger and echo GPIO
+# Задаем выходы GPIO для триггера и эхо
 GPIO_TRIGGER = 23
 GPIO_ECHO = 24
  
@@ -12,55 +14,60 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
  
 def distance():
-    # Send impulse on trigger 
+    # Посылаем импульс
     GPIO.output(GPIO_TRIGGER, True)
+    # Время задержки (если расстояние > 3 метров - выставляем время 25-30 мсек)	
     time.sleep(0.0001)
     GPIO.output(GPIO_TRIGGER, False)
-    # Init time var
+    # Инициализация переменных времени
     StartTime = time.time()
     StopTime = time.time()
  
-    # save time send signal
+    # Сохраняем время отправки сигнала
     while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
 #	print ("Start time: %.1f" % StartTime)
  
-    # save time recieved signal
+    # Сохраняем время получения сигнала
     while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
 #	print ("Stop time: %.1f" % StopTime)
  
-    # calc time
+    # Вычисляем время
     Time = StopTime - StartTime
-    # 
+    # Вычисляем расстояние 
     distance = (Time * 34300) / 2
  
     return distance
 
 def delta_distance():
+    # Порог срабатывания ( процент от усреднённого расстояния )
 	prcnt = 10
+    # Вычисляем среднее расстояние до объекта (50 замеров)
 	avrg = 0
 	for i in range(50):
 		dst = distance()
 		avrg += dst
 	avrg = avrg/50
+    # Если расстояние ниже delta - засчитывать проход
 	delta = avrg-(avrg/100.0*prcnt)
 
 	return delta
+
 	
 		
 	
 	
 
 if __name__ == '__main__':
-	avrg = average_distance()
+	avrg = delta_distance()
 	print (avrg)
 
 #	for i in range(10):
-#		avg = average_distance()
+#		avg = delta_distance()
 #		print (avg)
 #		dist = distance()
 #    		print ("Distance = %.1f cm" % avg)
 #    dat = count()
-#    print (dat)
+#    print (dat) выаыва
 	GPIO.cleanup()
